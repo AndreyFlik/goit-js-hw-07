09d3d38327a9f691cc296756e5e846552ea9013a
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-11-10 13:46:00 +0200

    добавил библиотеки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>;
