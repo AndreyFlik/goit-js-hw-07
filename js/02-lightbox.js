3ebaf4e855779b3ffb6956cd471136f60100f846
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-11-10 18:48:56 +0200

    нет ошибки

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const list = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map((image) => {
+    return `<a class="gallery__item" href="${image.original}">
+  <img
+    class="gallery__image"
+    src="${image.preview}"
+    alt="${image.description}"
+  />
+</a>`;
+  })
+  .join("");
+
+list.insertAdjacentHTML("beforeend", markup);
+
+const clickImg = document.querySelectorAll(".gallery__item");
+
+const clickImages = [...clickImg].map((img) => {
+  img.addEventListener("click", selectImg);
+
+  function selectImg(event) {
+    event.preventDefault();
+    const { dataset, alt } = event.target;
+    if (event.target.nodeName !== "IMG") {
+      return;
+    }
+    var lightbox = new SimpleLightbox(".gallery a", {
+      captionType: "attr",
+      captionsData: "alt",
+      captionDelay: 250,
+    });
+  }
+});
