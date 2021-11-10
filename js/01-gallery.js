0c54473ed0ca5a606c87604a2f56befc68acd792
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-11-10 14:11:57 +0200

    подключил изображения с разметкой

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,22 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
-<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div>;
+const body = document.body;
+// console.log(body);
+
+const list = body.firstElementChild;
+// console.log(list);
+
+const markup = galleryItems
+  .map((image) => {
+    return `<div class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image"
+src="${image.preview}"
+data-source="${image.original}"
+alt="${image.description}"/></a></div>`;
+  })
+  .join("");
+
+const imagesHtml = list.insertAdjacentHTML("afterend", markup);
+// console.log(markup);
